dae11a9be88b7ea40c6e153c7be4f79f1d6101bc
Adel <adel.aref16@gmail.com>
2018-04-02 16:45:33 -0700

    Initial Commit

---
+++ TaskApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaskApp
+//
+//  Created by Have a Mission on 4/2/18.
+//  Copyright © 2018 Have a Mission. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
